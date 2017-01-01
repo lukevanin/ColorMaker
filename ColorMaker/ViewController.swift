80d599203eb184ea242b33b697fdb7d8bfe83df8
Luke Van In <luke.vanin@takealot.com>
2017-01-01 17:38:04 +0200

    First commit. Added application files.

--- ColorMaker/ViewController.swift
+++ ColorMaker/ViewController.swift
@@ -9,17 +9,132 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    enum ChannelMode : Int {
+        case rgb
+        case hsb
+        case cmy
+        
+        func makeColorFor(x : CGFloat, y : CGFloat, z : CGFloat) -> UIColor {
+            switch self {
+            case .rgb:
+                return UIColor(red: x, green: y, blue: z, alpha: 1.0)
+                
+            case .hsb:
+                return UIColor(hue: x, saturation: y, brightness: z, alpha: 1.0)
+                
+            case .cmy:
+                return UIColor(red: 1.0 - x, green: 1.0 - y, blue: 1.0 - z, alpha: 1.0)
+            }
+        }
+    }
+    
+    @IBOutlet weak var xChannelLabel : UILabel!
+    @IBOutlet weak var yChannelLabel : UILabel!
+    @IBOutlet weak var zChannelLabel : UILabel!
+    @IBOutlet weak var xColorSlider : UISlider!
+    @IBOutlet weak var yColorSlider : UISlider!
+    @IBOutlet weak var zColorSlider : UISlider!
+    @IBOutlet weak var channelSelector : UISegmentedControl!
+    @IBOutlet weak var previewView : UIView!
+    
+    private var channelMode : ChannelMode = .rgb
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateChannels()
+        updateColor()
+        updateLabels()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onSliderValueChanged(_ sender: Any) {
+        updateColor()
+        updateLabels()
     }
-
-
+    
+    @IBAction func onChannelChanged(_ sender: Any) {
+        resetSliders()
+        updateChannels()
+        updateColor()
+        updateLabels()
+    }
+    
+    private func resetSliders() {
+        xColorSlider.value = 0.5
+        yColorSlider.value = 0.5
+        zColorSlider.value = 0.5
+    }
+    
+    private func updateChannels() {
+        channelMode = ChannelMode(rawValue: channelSelector.selectedSegmentIndex)!
+    }
+    
+    private func updateColor() {
+        let color = makeColor()
+        previewView.backgroundColor = color
+        view.backgroundColor = makeFadedColor(from: color)
+    }
+    
+    private func updateLabels() {
+        let xChannelName : String
+        let yChannelName : String
+        let zChannelName : String
+        
+        switch channelMode {
+            
+        case .rgb:
+            xChannelName = "RED"
+            yChannelName = "GREEN"
+            zChannelName = "BLUE"
+            
+        case .hsb:
+            xChannelName = "HUE"
+            yChannelName = "SATURATION"
+            zChannelName = "BRIGHTNESS"
+            
+        case .cmy:
+            xChannelName = "CYAN"
+            yChannelName = "MAGENTA"
+            zChannelName = "YELLOW"
+        }
+        
+        xChannelLabel.text = "\(xChannelName) \(Int(xColorSlider.value * 100))%"
+        yChannelLabel.text = "\(yChannelName) \(Int(yColorSlider.value * 100))%"
+        zChannelLabel.text = "\(zChannelName) \(Int(zColorSlider.value * 100))%"
+    }
+    
+    private func makeColor() -> UIColor {
+        return channelMode.makeColorFor(
+            x: CGFloat(xColorSlider.value),
+            y: CGFloat(yColorSlider.value),
+            z: CGFloat(zColorSlider.value)
+        )
+    }
+    
+    private func makeFadedColor(from color: UIColor) -> UIColor {
+        let delta = CGFloat(0.25)
+        var r : CGFloat = 0
+        var g : CGFloat = 0
+        var b : CGFloat = 0
+        
+        guard color.getRed(&r, green: &g, blue: &b, alpha: nil) else {
+            return UIColor.black
+        }
+        
+        return UIColor(
+            red: r * delta,
+            green: g * delta,
+            blue: b * delta,
+            alpha: 1.0
+        )
+    }
 }
 
